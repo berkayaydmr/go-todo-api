b08ba75913cfa8557b3a8151c2b062506507db37
berkay <20comp1028@isik.edu.tr>
2022-03-16 15:55:04 +0300

    fix version and table problems migration

--- migrations/000001_go-todo-api.up.sql
+++ migrations/000001_go-todo-api.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE to_dos (
     "details" varchar NOT NULL,
     "status" varchar NOT NULL,
     "created_at" timestamp NOT NULL DEFAULT (now()),
-    "updated_at" timestamp NOT NULL DEFAULT (now()),
+    "updated_at" timestamp NOT NULL DEFAULT (now())
 );
