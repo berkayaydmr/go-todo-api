63506d9ebf6b1ad476317143c7f27a5c1757c3ae
berkay <20comp1028@isik.edu.tr>
2022-03-16 14:29:20 +0300

    fix todo_entity

--- entities/todo_entity.go
+++ entities/todo_entity.go
@@ -5,9 +5,9 @@ import "time"
 type ToDo struct {
 	Id        uint64    `gorm:"primaryKey;column:todo_id"`
 	UserId    uint64    `gorm:"column:user_id"`
+	User      User      `gorm:"foreingKey:UserId;"`
 	Details   string    `gorm:"column:details;"`
 	Status    string    `gorm:"column:status;default:To-Do"`
 	CreatedAt time.Time `gorm:"column:created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at"`
-	User      User      `gorm:"foreingKey:UserId;"`
 }
